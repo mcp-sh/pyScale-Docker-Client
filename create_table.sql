a1d4ce9d76f85ab3e7bd2ea2c78efee101bd3f9b
MCP <mcp@macapa.me>
2021-10-12 15:26:55 +0800

    Updated SQL

--- create_table.sql
+++ create_table.sql
@@ -1,5 +1,6 @@
+DROP TABLE IF EXISTS `scales`; 
 CREATE TABLE `scales` (
-  `batch` bigint unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `batch_id` bigint unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `timestamp` varchar(255) NOT NULL,
   `scale_id` tinyint NOT NULL,
   `gross_kg` float NOT NULL,
