a2bd683d62811a4316d0af524f9195cce5da4a06
Office - MCP <mcp@macapa.me>
2021-10-13 13:06:16 +0800

    Cleanup and working restarting client

--- MT-client.py
+++ MT-client.py
@@ -7,28 +7,19 @@ import re
 import os.path
 from mysql.connector import connect, Error
 
-
-# Setting the host and port number from CLI arguments
-# Second parameter for port is optional
-
-# HOST = sys.argv[1]
-# if len(sys.argv) > 2:
-#     PORT = int(sys.argv[2])
-# else:
-#     PORT = 1025        # Default port for MT IND360 controller
-
-#
-# Setting HOST and PORT from ENV variables provided through Docker Compose
-#
-
 HOST = os.environ['SCALE_HOST']
 PORT = int(os.environ['SCALE_PORT'])
 SCALE = int(os.environ['SCALE_NUM'])
 
+# Hard coded values for testing
+# HOST = '127.0.0.1'
+# PORT = 1033
+# SCALE = 1
+
 print(HOST)
 print(PORT)
 
-# Converison factor to convert kg to pounds
+# Conversion factor to convert kg to pounds
 KGTOPOUNDS = 2.2046
 
 
@@ -57,12 +48,12 @@ def createquery(value):
     return res
 
 
-def logweight(value):
+def log_to_csv(value):
     needs_header = True
     csv_columns = ['Scale', 'Timestamp',
                    'Gross (kg)', 'Net (kg)', 'Tare (kg)', 'Gross (lb)', 'Net (lb)', 'Tare (lb)']
     datestring = datetime.datetime.today().strftime("%Y%m%d")
-    logfile = '/scaledata/log-'+datestring+'.csv'
+    logfile = './scaledata/log-'+datestring+'.csv'
 
     if os.path.isfile(logfile):
         needs_header = False
@@ -101,17 +92,35 @@ def cleanString(input):
         'Gross (lb)': round((remove_kg(values[1])*KGTOPOUNDS), 2),
         'Tare (lb)': round((remove_kg(values[3])*KGTOPOUNDS), 2)
     }
-    # print(values_dict)
     return values_dict
 
+def handle_data(msg):
+    value = cleanString(msg)
+    print(value)
+    log_to_csv(value)
+    sqlquery = createquery(value)
+    logtodb(sqlquery)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    while True:
-        data = s.recv(1024)
-        msg = data.decode('utf-8')
-        value = cleanString(msg)
-        print(value)
-        logweight(value)
-        sqlquery = createquery(value)
-        logtodb(sqlquery)
+def connect_to_scale(s):
+    try:
+        s.connect((HOST, PORT))
+    except socket.error as e:
+        print(str(e))
+    res = s.recv(512)
+    rstr = res.decode('utf-8')
+    handle_data(rstr)
+    match = re.search('\*{5,30}', rstr )
+    if match:
+        print('Message complete')
+        print('Closing Connection')
+        s.close()
+
+rounds = 1
+
+while True:
+    ClientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print(f'Attempt number: {rounds}')
+    connect_to_scale(ClientSocket)
+    # time.sleep(3)
+    rounds += 1
+
